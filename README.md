cf15680c7cc08ad23f33db794a35306ba20b855b
Rajat <robertdoweny2301@gmail.com>
2021-12-07 23:43:05 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# The `best` testing framework
+
---
+++ index.mjs
@@ -0,0 +1,66 @@
+import JestHasteMap from 'jest-haste-map';
+import {cpus} from 'os';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import {Worker} from 'jest-worker';
+import {join, relative} from 'path';
+import chalk from 'chalk';
+
+// Get the root path to our project (Like `__dirname`).
+const root = dirname(fileURLToPath(import.meta.url));
+const hasteMap = new JestHasteMap.default({
+  extensions: ['js'],
+  maxWorkers: cpus().length,
+  name: 'test-framework',
+  platforms: [],
+  rootDir: root,
+  roots: [root],
+});
+
+// Build and return an in-memory HasteFS ("Haste File System") instance.
+const {hasteFS} = await hasteMap.build();
+const testFiles = hasteFS.matchFilesWithGlob([
+  process.argv[2] ? `**/${process.argv[2]}*` : '**/*.test.js',
+]);
+
+
+
+// / enabling worker thread --> to parallelize tests across threads within the same process
+const worker = new Worker(join(root, 'worker.js'), {
+  enableWorkerThreads: true,
+});
+let hasFailed = false;
+
+await Promise.all(Array.from(testFiles).map(async testFile => {
+  const {success, testResults, errorMessage} = await worker.runTest(testFile);
+  const status = success
+    ? chalk.green.inverse.bold(' PASS ')
+    : chalk.red.inverse.bold(' FAIL ');
+
+  console.log(status + ' ' + chalk.dim(relative(root, testFile)));
+  if (!success) {
+    hasFailed = true;
+    if (testResults) {
+      testResults
+        .filter((result) => result.errors.length)
+        .forEach((result) =>
+          console.log(
+            result.testPath.slice(1).join(' ') + '\n' + result.errors[0],
+          ),
+        );
+    } else if (errorMessage) {
+      console.log('  ' + errorMessage);
+    }
+  }
+}));
+
+worker.end();
+
+
+if (hasFailed) {
+  console.log(
+    '\n' + chalk.red.bold('Test run failed, please fix all the failing tests.'),
+  );
+  // Set an exit code to indicate failure.
+  process.exitCode = 1;
+}
